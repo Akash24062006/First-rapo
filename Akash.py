0adac86c0b4276c491031421a0e50b997518be17
Akash24062006 <daak3903@gmail.com>
2026-08-27 11:43:38 +0530

    Add files via upload

---
+++ Akash.py
@@ -0,0 +1,90 @@
+#Exercise 8
+numbers = [10,20,30,40,50]
+print(numbers[4])
+
+
+#exercise 5
+message = "Welcome to Python"
+print(message)
+#The error is a missing closing Quotation mark
+
+
+#Excise 6
+num1 = 100
+num2 = 0
+
+if num2 != 0:
+    result = num1 / num2
+    print(result)
+else:
+    print("Cannot divide by zero")
+    
+    
+    #exercise 7
+    number = [10,20,30,40,50]
+    print(numbers[0])
+    
+    #exercise 8
+    
+    student = {
+        "name" : "Rahul",
+        "age" : 20
+    }
+    
+    #exercise 10
+    age = int(input("Enter your age: "))
+    print("Your age is:", age)
+    print("Invalid input. Please enter a valid number.")
+    
+    #exercise 11
+    a = 20
+b = 30
+result = a + b
+print("The sum is:", result)
+
+#exercise 12
+length = 10
+width = 5
+area = length * width  # Changed operator from + to *
+print("Area =", area)
+
+#execise 13
+maths = 80
+scince = 70
+english = 90
+average = (maths + scince + english) / 3
+print("Average marks:", average)
+
+#exercise 14
+number = 7
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+    
+    #execise 15
+    
+    a = 50
+b = 80
+
+if a > b:
+    print("Largest:", a)
+else:
+    print("Largest:", b)
+    
+    #exercise 16
+    name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Name:", name)
+print("Age:", age)
+
+#execrise 17
+marks = int(input("Enter marks: "))
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+    
